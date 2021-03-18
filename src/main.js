bbd36f49c39b5af3a42c82d1d8fcf46a7e5a4568
Mohamed Ramadan <m7mdrama1245@gmail.com>
2021-03-18 03:20:21 +0200

    add Global filter

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
+import { mapValues } from "async";
+import Vue from "vue";
+import App from "./App.vue";
 
-Vue.filter('to-lowercase');
+Vue.filter("to-lowercase", function(value) {
+  return value.toLowerCase();
+});
 
 new Vue({
-  el: '#app',
+  el: "#app",
   render: h => h(App)
-})
+});
